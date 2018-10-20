91b8d348804172db113ba3ef52202520b345bb0c
whage <whage@freemail.hu>
2018-10-20 13:51:48 +0200

    finish exercise_2

--- 2_print_patterns.cpp
+++ 2_print_patterns.cpp
@@ -39,6 +39,22 @@ grid exercise_1(grid g) {
     return g;
 }
 
+grid exercise_2(grid g) {
+    for (int i = 0; i < g.height; i++) {
+        int space_count = i;
+
+        for (int j = 0; j < g.width - space_count; j++) {
+            g.cells[i][j] = g.width - space_count - j + 48;
+        }
+        
+        for (int s = g.width - space_count; s < g.width; s++) {
+            g.cells[i][s] = ' ';
+        }
+    }
+
+    return g;
+}
+
 grid allocate_grid(int width, int height) {
     grid g;
     g.width = g.height = 8;
@@ -55,4 +71,5 @@ grid allocate_grid(int width, int height) {
 
 int main() {
     print_grid(exercise_1(allocate_grid(8, 8)));
+    print_grid(exercise_2(allocate_grid(8, 8)));
 }
