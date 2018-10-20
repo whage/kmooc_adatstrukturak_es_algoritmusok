4e8f60d75dc48a2f6b8efdf9886b7a64e2437c63
whage <whage@freemail.hu>
2018-10-20 13:22:37 +0200

    refactor: add allocate_grid function, rename variables

--- 2_print_patterns.cpp
+++ 2_print_patterns.cpp
@@ -3,18 +3,18 @@
 
 using namespace std;
 
-struct pattern {
+struct grid {
     int width;
     int height;
-    char **grid;
+    char **cells;
 };
 
-void print_pattern(pattern p) {
+void print_grid(grid g) {
     // iterate each line
-    for (int i = 0; i < p.height; i++) {
+    for (int i = 0; i < g.height; i++) {
         // iterate each char in current line
-        for (int j = 0; j < p.width; j++) {
-            cout << p.grid[i][j];
+        for (int j = 0; j < g.width; j++) {
+            cout << g.cells[i][j];
         }
 
         // add new line character
@@ -22,33 +22,37 @@ void print_pattern(pattern p) {
     }
 }
 
-pattern exercise_1() {
-    pattern p;
-    p.width = p.height = 8;
-
-    // allocate memory for grid: 8 * 8 * sizeof(char)
-    p.grid = (char**)malloc(sizeof(char*) * p.width);
-
-    for (int i = 0; i < p.height; i++) {
-        p.grid[i] = (char*)malloc(sizeof(char*));
-    }
-
-    for (int i = 0; i < p.height; i++) {
+grid exercise_1(grid g) {
+    for (int i = 0; i < g.height; i++) {
         int space_count = i;
         
         for (int s = 0; s < space_count; s++) {
-            p.grid[i][s] = ' ';
+            g.cells[i][s] = ' ';
         }
 
-        for (int j = space_count; j < p.width; j++) {
+        for (int j = space_count; j < g.width; j++) {
             // convert int to corresponding numeric character
-            p.grid[i][j] = j - space_count + 49;
+            g.cells[i][j] = j - space_count + 49;
         }
     }
 
-    return p;
+    return g;
+}
+
+grid allocate_grid(int width, int height) {
+    grid g;
+    g.width = g.height = 8;
+
+    // allocate memory for grid: 8 * 8 * sizeof(char)
+    g.cells = (char**)malloc(sizeof(char*) * g.width);
+
+    for (int i = 0; i < g.height; i++) {
+        g.cells[i] = (char*)malloc(sizeof(char*));
+    }
+
+    return g;
 }
 
 int main() {
-    print_pattern(exercise_1());
+    print_grid(exercise_1(allocate_grid(8, 8)));
 }
